36900a4d218c439ec8c59652770a17abf1c8869e
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-03-27 13:02:21 +0530

    Update Google_existing_user_sign_in.java

--- src/test/java/SetupClass/TestStep/Google_existing_user_sign_in.java
+++ src/test/java/SetupClass/TestStep/Google_existing_user_sign_in.java
@@ -51,10 +51,10 @@ public class Google_existing_user_sign_in extends Set {
 
 	@Then("^User click on sign in with google plus button old gmail$")
 	public void user_click_on_sign_in_with_google_plus_button_old_gmail() throws InterruptedException  {
-		Thread.sleep(4000);
+		Thread.sleep(8000);
 		
 		WebElement gmail_email = wait.until(ExpectedConditions.elementToBeClickable(By.ID("Email")));
-		Thread.sleep(2000);
+		Thread.sleep(4000);
 	    gmail_email.sendKeys("himanshi.sharma@slidetech.in");
 	    Thread.sleep(2000);
 		
